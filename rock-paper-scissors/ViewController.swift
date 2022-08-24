398c5674315411270cc51328de2102d13672e47e
Asim Can Yagiz <meycasim@gmail.com>
2022-08-24 16:50:19 +0300

    UI connected with view controller

--- rock-paper-scissors/ViewController.swift
+++ rock-paper-scissors/ViewController.swift
@@ -8,12 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    
+    @IBOutlet weak var enemyMove: UIImageView!
+    @IBOutlet weak var playerMove1: UIImageView!
+    @IBOutlet weak var playerMove2: UIImageView!
+    @IBOutlet weak var playerMove3: UIImageView!
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    
+    
+    
+    
+    
+    
 
 }
 
